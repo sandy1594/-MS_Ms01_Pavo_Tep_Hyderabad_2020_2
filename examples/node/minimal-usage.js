bacf2c51ec90d0149c97b8b5240e81dc51f3b649
sandy1594 <burrasandeepkumar1994@gmail.com>
2020-05-03 19:47:16 +0530

    first commit

--- examples/node/minimal-usage.js
+++ examples/node/minimal-usage.js
@@ -5,13 +5,14 @@ faker.locale = "en";
 
 //console.log(faker.lorem.sentences())
 
-console.log(faker.name.findName())
-return;
+//console.log(faker.name.findName())
+//return;
 //console.log(faker.address)
-console.log(faker.internet.email())
-console.log(faker.date.recent())
-console.log(faker.helpers.contextualCard());
+//console.log(faker.internet.email())
+//console.log(faker.date.recent())
+//console.log(faker.helpers.contextualCard());
 
-faker.locale = "uk";
+//faker.locale = "uk";
 
-console.log(faker.helpers.contextualCard());
+//console.log(faker.helpers.contextualCard());//
+console.log(faker.laptop.lappymodel());
